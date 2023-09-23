e3432ad6bd65bf02f4a5f904c0eef7c2f3ca5d5d
Joshua <Joshua.soto@live.com>
2023-09-22 19:32:24 -0500

    added var for todays date

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+var  todaysDate = moment().format('dddd, MMM Do YYYY');
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(function (timeTracker) {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
