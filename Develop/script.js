98399ebb8c3853ec0b0a3410014a50b13eaeff7c
Joshua <Joshua.soto@live.com>
2023-09-24 19:53:38 -0500

    more changes to java

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
-var  todaysDate = moment().format('dddd, MMM, Do YYYY');
-$('#todaysDate').html(todaysDate);
-
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
 $(document).ready(function () {
-  $('saveBtn').on('click', function () {
+    //  click listener 
+    $(".saveBtn").on("click", function () {
 
     var text = $(this).siblings('description').val();
     var time = $(this).parent().attr('id');
@@ -11,19 +11,31 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   })
 
+  $('#hour8 .description').val(localStorage.getItem('hour8'));
+  $('#hour9 .description').val(localStorage.getItem('hour9'));
+  $('#hour10 .description').val(localStorage.getItem('hour10'));
+  $('#hour11 .description').val(localStorage.getItem('hour11'));
+  $('#hour12 .description').val(localStorage.getItem('hour12'));
+  $('#hour13 .description').val(localStorage.getItem('hour13'));
+  $('#hour14 .description').val(localStorage.getItem('hour14'));
+  $('#hour15 .description').val(localStorage.getItem('hour15'));
+  $('#hour16 .description').val(localStorage.getItem('hour16'));
+  $('#hour17 .description').val(localStorage.getItem('hour17'));
+  
+
   function timeKeeper() {
 
-  var currentTime = moment().hour();
+  var currentHour = moment().hour();
 
   $('.time-block').each(function () {
 var blockTime = parseInt($(this).attr('id').split('hour')[1]);
 
-if(blockTime < currentTime) {
+if (blockTime < currentHour) {
    $(this).removeClass('future');
    $(this).removeClass('present');
    $(this).addClass('past');
 }
-else if(blockTime < currentTime) {
+else if (blockTime === currentHour) {
   $(this).removeClass('future');
   $(this).removeClass('present');
   $(this).addClass('past');
@@ -37,17 +49,5 @@ else {
   })
 }
 
-
-$('#hour8 .description').val(localStorage.getItem('hour8'))
-$('#hour9 .description').val(localStorage.getItem('hour9'))
-$('#hour10 .description').val(localStorage.getItem('hour10'))
-$('#hour11 .description').val(localStorage.getItem('hour11'))
-$('#hour12 .description').val(localStorage.getItem('hour12'))
-$('#hour13 .description').val(localStorage.getItem('hour13'))
-$('#hour14 .description').val(localStorage.getItem('hour14'))
-$('#hour15 .description').val(localStorage.getItem('hour15'))
-$('#hour16 .description').val(localStorage.getItem('hour16'))
-$('#hour17 .description').val(localStorage.getItem('hour17'))
-
-timeKeeper();
+  timeKeeper();
 })
